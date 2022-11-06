e11d313252081192487f0cd36573cff5009466a3
Diana <diana.wanjiru@student.moringaschool.com>
2022-11-06 23:57:45 +0300

    more progress

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,20 @@ class Product < ActiveRecord::Base
    def leave_review(user, star_rating, comment)
     Review.create(star_rating:star_rating, comment:comment, product_id:self.id, user_id:user.id)
    end
+   #should `puts` to the terminal a string representing each review for this product
+   #each review should be formatted as follows: `Review for {insert product name} by {insert user name}: {insert review star_rating}. {insert review comment}`
+   def print_all_reviews
+    self.reviews.each do |review|
+        review.print_review
+    end
+   end
+
+    
+    #returns a `float` representing the average star rating for all reviews 
+    #for this product
+    def average_rating
+        self.reviews.average('star_rating').to_f
+    end
+
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
 
+    #returns the product instance that has the highest star rating from this user
+
     def favorite_product
+        self.products.order('star_rating ASC').last
+    end
+
+    #takes a `Product` (an instance of the `Product` class) and removes _all_ of this 
+    #user's reviews for that product
+   #you will have to delete any rows from the `reviews` table associated with this user and the product
+
+    def remove_reviews(product)
+        self.reviews.where(product:product).destroy_all
     end
     
 end
